27b0e35f39557690df1c62ae3764038aeff4df8e
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-26 16:13:43 -0600

    feat: Add Private Readonly Fields

--- MathByDatesApp/MathByDates/DateHelper.cs
+++ MathByDatesApp/MathByDates/DateHelper.cs
@@ -1,5 +1,7 @@
 ﻿namespace MathByDates;
 public static class DateHelper
 {
-
+    private static readonly DateOnly _firstDate = new();
+    private static readonly DateOnly _secondDate = new();
+    private static readonly int _daysToAdd = Random.Shared.Next(20, 51);
 }
