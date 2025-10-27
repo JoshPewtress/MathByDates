68557c059bcaa3a71bc59e45828961447f599ed5
Josh Pewtress <61856857+JoshPewtress@users.noreply.github.com>
2025-10-27 10:31:03 -0600

    fix: Updated verbage in user instructions - Refined game instructions to be more clear

--- MathByDatesApp/MathByDates/Program.cs
+++ MathByDatesApp/MathByDates/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine(
     """
     Math by Dates -- A C# date guessing game
     You will be shown a target number of days
-    You will guess two dates and the amount of days from today will be totalled.
+    You will guess two dates and the amount of days from today to your guesses will be totalled.
     If the total number of days for your guesses are within 5 of the target, You Win!
 
     """);
